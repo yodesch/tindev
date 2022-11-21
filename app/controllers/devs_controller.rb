de54ffb9a683c790ffe2da3caf0637035cf7a7f8
Justine Di Caro <justinedicaro@gmail.com>
2022-11-21 17:51:44 +0100

    controller + model

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -1,2 +1,30 @@
 class DevsController < ApplicationController
+  before_action :set_dev
+
+  def new
+    @dev = Dev.new
+  end
+
+  def create
+    @dev = Dev.new(dev_params)
+    if @dev.save
+      redirect_to dev_path(@dev)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @dev.destroy
+  end
+
+  private
+
+  def set_dev
+    @dev = Dev.find(params[:id])
+  end
+
+  def user_params
+    params.require(:dev).permit(:name, :age, :city, :language, :disponibility, :price_per_day, :rating, :avatar_url)
+  end
 end
